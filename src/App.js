9bb7f2a6a20a4f783e6d9aefc89a5393e3450bc0
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-10-18 21:46:42 +0100

    Updated app.js and index,js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import './CSS/index.css';
+import Home from './components/Home';
+import DetailsPage from './components/DetailsPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="details/:name" element={<DetailsPage />} />
+      </Routes>
     </div>
   );
 }
